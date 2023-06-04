0071a01e949240dae009e2afd38535334fc8c551
DiegoCraft26 <alejandromorenod@yahoo.com>
2023-06-04 13:23:32 -0600

    test logo

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ st.title("Taller de Informatica y Matematicas")
 
 #Header
 st.header("Preparatoria 9")
+
+#add a logo in the left corner
+from PIL import Image
+image = Image.open('prepa9logo.png')
+st.image(image, width=100)
